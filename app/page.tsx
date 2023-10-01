86d0ecc2b17fd58e8ab80e00f958b8a4dd4ab1a9
Anushree <anushreejaiswal.srn@gmail.com>
2023-10-01 05:52:36 +0530

    removed ui elements

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,7 @@
-import { ModeToggle } from "@/components/theme-toggle";
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
 	return (
-		<main>
-			<h1>Elenco</h1>
-			<Button>Example Button</Button>
-
-			<ModeToggle />
+		<main className="bg-primary text-secondary min-h-screen">
+			<h1 className="">Elenco</h1>
 		</main>
 	);
 }
