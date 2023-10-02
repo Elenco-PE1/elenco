de1b10369cd1a45ca2cc40299ccf7eb124ad8fb6
Anushree <anushreejaiswal.srn@gmail.com>
2023-10-02 17:03:59 +0530

    updated components styling

--- components/ui/sheet.tsx
+++ components/ui/sheet.tsx
@@ -34,14 +34,14 @@ const SheetOverlay = React.forwardRef<
 SheetOverlay.displayName = SheetPrimitive.Overlay.displayName;
 
 const sheetVariants = cva(
-	"fixed z-10 gap-4 bg-foreground text-secondary pt-16 pl-12 pr-2 shadow-lg transition ease-in-out data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:duration-300 data-[state=open]:duration-500",
+	"fixed z-10 gap-4 bg-foreground text-secondary pt-16 pb-12 shadow-lg transition ease-in-out data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:duration-300 data-[state=open]:duration-500",
 	{
 		variants: {
 			side: {
 				top: "inset-x-0 top-0 border-b data-[state=closed]:slide-out-to-top data-[state=open]:slide-in-from-top",
 				bottom: "inset-x-0 bottom-0 border-t data-[state=closed]:slide-out-to-bottom data-[state=open]:slide-in-from-bottom",
 				left: "inset-y-0 left-0 h-full w-[250px] border-r data-[state=closed]:slide-out-to-left data-[state=open]:slide-in-from-left sm:max-w-sm",
-				right: "inset-y-0 right-0 h-full w-3/4  border-l data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right sm:max-w-sm",
+				right: "inset-y-0 right-0 h-full w-[320px] border-l data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right sm:max-w-sm",
 			},
 		},
 		defaultVariants: {
