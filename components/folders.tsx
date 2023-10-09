738cb834e9f8b597faf6e5e15f766bd05e3533a7
Anushree <anushreejaiswal.srn@gmail.com>
2023-10-09 20:48:50 +0530

    converted files into tabs with relevant tab content

--- components/folders.tsx
+++ components/folders.tsx
@@ -1,5 +1,5 @@
 import { ChevronRightIcon, ChevronDownIcon } from "lucide-react";
-import { useState } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 import { Button } from "./ui/button";
 import {
 	Collapsible,
@@ -7,46 +7,80 @@ import {
 	CollapsibleTrigger,
 } from "./ui/collapsible";
 import { Separator } from "./ui/separator";
+import { Tabs, TabsContent, TabsTrigger } from "./ui/tabs";
+import { TabsList } from "@radix-ui/react-tabs";
+import { Workspace } from "./workspace";
 
-export const Folder = ({value}: {value: number}) => {
+export const Folder = ({
+	folderIndex,
+	ar,
+	file,
+	currentTab,
+	setCurrentFile,
+	setCurrentFolder,
+	settingTab,
+}: {
+	folderIndex: number;
+	ar: number[][];
+	file: number[];
+	currentTab: number;
+	setCurrentFile: Dispatch<SetStateAction<number>>;
+	setCurrentFolder: Dispatch<SetStateAction<number>>;
+	settingTab: number[],
+}) => {
 	const [isOpen, setIsOpen] = useState(false);
-	const ar = [1, 2, 3, 4, 5, 6];
 
 	return (
-		<Collapsible open={isOpen} onOpenChange={setIsOpen} className="w-full">
-			<div className="flex items-center gap-2">
-				<CollapsibleTrigger asChild>
-					<Button variant="ghost" size="sm" className="p-0">
-						{!isOpen ? (
-							<ChevronRightIcon
-								width={18}
-								className="transition-all"
-							/>
-						) : (
-							<ChevronDownIcon
-								width={18}
-								className="transition-all"
-							/>
-						)}
-					</Button>
-				</CollapsibleTrigger>
-				<h4 className="text-sm whitespace-nowrap font-semibold">
-					Team Documentation {value}
-				</h4>
-			</div>
-			<CollapsibleContent className="transition-all">
-				{ar.map((value, id) => (
-					<div
-						className="transition-all flex h-7 items-center ml-6 space-x-2 text-sm"
-						key={id}
-					>
-						<Separator orientation="vertical" />
-						<p className="text-sm flex items-center rounded-sm w-full h-full pl-2 hover:bg-muted-foreground hover:cursor-pointer">
-							Document {value}
-						</p>
-					</div>
-				))}
-			</CollapsibleContent>
-		</Collapsible>
+		<Tabs defaultValue={`${settingTab[0]}`}>
+			<Collapsible
+				open={isOpen}
+				onOpenChange={setIsOpen}
+				className="w-full"
+			>
+				<div className="flex items-center gap-2">
+					<CollapsibleTrigger asChild>
+						<Button variant="ghost" size="sm" className="p-0">
+							{!isOpen ? (
+								<ChevronRightIcon
+									width={18}
+									className="transition-all"
+								/>
+							) : (
+								<ChevronDownIcon
+									width={18}
+									className="transition-all"
+								/>
+							)}
+						</Button>
+					</CollapsibleTrigger>
+					<h4 className="text-sm whitespace-nowrap font-semibold">
+						Team Documentation {folderIndex+1}
+					</h4>
+				</div>
+				<CollapsibleContent className="transition-all">
+					<TabsList>
+						{file.map((filevalue, id) => (
+							<TabsTrigger
+								value={`${ar[folderIndex][id]}`}
+								className="transition-all flex h-7 items-center rounded-l-none rounded-r-md ml-6 space-x-2 text-sm bg-transparent p-0 data-[state=active]:bg-muted-foreground hover:bg-muted-foreground  hover:cursor-pointer"
+								key={id}
+								defaultChecked={ id==0 && settingTab[currentTab] == 0 }
+								onClick={() => {
+									setCurrentFile(id);
+									setCurrentFolder(folderIndex);
+									let temp = settingTab
+									temp[currentTab] = id
+								}}
+							>
+								<Separator orientation="vertical" />
+								<p className="text-sm flex items-center rounded-sm w-full h-full pl-2">
+									Document {filevalue}
+								</p>
+							</TabsTrigger>
+						))}
+					</TabsList>
+				</CollapsibleContent>
+			</Collapsible>
+		</Tabs>
 	);
 };
