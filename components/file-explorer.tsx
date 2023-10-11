cc84812409e6e4fe39d36c9245dcb431ff234877
Anushree <anushreejaiswal.srn@gmail.com>
2023-10-11 06:14:42 +0530

    updated props

--- components/file-explorer.tsx
+++ components/file-explorer.tsx
@@ -5,28 +5,32 @@ import { Folder } from "./folders";
 import { ScrollArea } from "./ui/scroll-area";
 
 export const FileExplorer = ({
-	ar,
+	teamDoc,
+	newFile,
+	newFolder,
 	currentTab,
 	setCurrentFile,
 	setCurrentFolder,
 	settingTab,
 }: {
-	ar: number[][];
+	teamDoc: number[][],
+	newFile: number[],
+	newFolder: number,
 	currentTab: number;
 	setCurrentFile: Dispatch<SetStateAction<number>>;
 	setCurrentFolder: Dispatch<SetStateAction<number>>;
 	settingTab: number[],
 }) => {
 	return (
-		<div className="w-[220px] border-r-[1px] border-muted-foreground h-screen mt-16 ml-10 p-2 bg-primary text-muted">
+		<div className="w-[220px] border-r-[1px] border-muted-foreground h-screen mt-16 ml-10 p-2 bg-primary text-muted transition-opacity ease-in-out">
 			<h3 className="font-medium text-lg mt-2 mb-2">Team Vault</h3>
 			<ScrollArea className="h-[575px]">
-				{ar.map((value, id) => (
+				{teamDoc.slice(0, newFolder).map((value, id) => (
 					<Folder
 						key={id}
 						folderIndex={id}
-						file={value}
-						ar={ar}
+						teamDoc={teamDoc}
+						newFile={newFile}
 						currentTab={currentTab}
 						setCurrentFile={setCurrentFile}
 						setCurrentFolder={setCurrentFolder}
