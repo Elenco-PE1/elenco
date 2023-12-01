fc940627ff4b7e6975efc06f711e496eb2a65ca9
Anushree <anushreejaiswal.srn@gmail.com>
2023-12-01 11:40:23 +0530

    removed clerk components

--- components/log-out-dialog.tsx
+++ components/log-out-dialog.tsx
@@ -1,18 +1,15 @@
 "use client";
-import { useAuth } from "@clerk/nextjs";
 import { Button } from "./ui/button";
 import { DialogFooter, DialogContent, DialogTitle } from "./ui/dialog";
 
 export const LogOutDialog = () => {
-	const { signOut } = useAuth();
-
 	return (
 		<DialogContent className="flex flex-col items-center">
 			<DialogTitle className="text-center">
 				Are you sure you want to log out?
 			</DialogTitle>
 			<DialogFooter>
-				<Button variant={"destructive"} onClick={() => signOut()}>
+				<Button variant={"destructive"} onClick={() => console.log("sign out")}>
 					Log Out
 				</Button>
 			</DialogFooter>
--- components/profile-box.tsx
+++ components/profile-box.tsx
@@ -1,19 +1,14 @@
 "use client";
 
-import { useUser } from "@clerk/nextjs";
 import { DialogContent, DialogTitle, DialogDescription } from "./ui/dialog";
 import { Button } from "./ui/button";
 import Link from "next/link";
 
 export const ProfileBox = () => {
-	const { isSignedIn, isLoaded, user } = useUser();
-
-	if (!isSignedIn || !isLoaded || !user) return null;
-
 	return (
 		<DialogContent className="flex flex-col items-center">
 			<DialogTitle className="text-center">
-				{user.firstName}'s Profile
+				Anushree's Profile
 			</DialogTitle>
 
 			<DialogDescription>
