1fb28a645239ab77d484ce7857e6f617110c6363
Anushree <anushreejaiswal.srn@gmail.com>
2023-12-02 11:47:48 +0530

    updated font weights

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Poppins } from "next/font/google";
 
 const poppins = Poppins({
 	subsets: ["latin"],
-	weight: "300",
+	weight: ["300","400","500","600","700","800","900"],
 });
 
 export const metadata: Metadata = {
